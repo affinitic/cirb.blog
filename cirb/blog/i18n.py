d1f72a1c2a2355ded14760e192e2899417177e52
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-12-14 14:32:38 +0100

    update translations

--- cirb/blog/i18n.py
+++ cirb/blog/i18n.py
@@ -3,7 +3,10 @@ from zope.i18nmessageid.message import MessageFactory
 _ = MessageFactory("cirb.blog")
 
 # add here msgid from template
-_(u"Posted in ")
+_(u"Posted in")
 _(u"View all posts in ")
 _(u"Tagged")
 _(u"Leave a reply")
+_(u"Posted on")
+_(u"by")
+_(u"Reply")
